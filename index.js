87a989b30f03ba56d6561e74b931cfafa04bd733
Anthony P Tetlow <anthony.tetlow@thedrum.com>
2016-04-05 17:42:36 +0100

    Improved the styleguide plugin

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ var PluginError = gutil.PluginError;
 var File = gutil.File;
 var dss = require('dss');
 var Twig = require('twig');
+
+// Disable twig caching
+Twig.cache(false);
+
 var twig = Twig.twig;
 
 
 // file can be a vinyl file object or a string
 // when a string it will construct a new one
-module.exports = function(file, opt) {
+module.exports = function(opt) {
   // if (!file) {
   //   throw new PluginError('gulp-concat', 'Missing file option for gulp-concat');
   // }
+
 
     opt = opt || {};
 
@@ -39,39 +44,60 @@ module.exports = function(file, opt) {
 
         var temp;
 
-        dss.parse(file.contents.toString(), {}, function (parsed) {
+        dss.parse(file.contents.toString(), {}, function (page) {
 
-            // Add filename
-            parsed.file = file.path
 
-            styleguide.push(parsed)
+            page.blocks = page.blocks.filter(function (block) {
+                return typeof block['styleguide'] !== 'undefined';
+            });
+
+            if(page.blocks.length > 0) {
+
+
+                // Add filename
+                page.file = file.path
+
+                if(opt.transformFileName) {
+                    page.url = opt.transformFileName(page.file)
+                } else {
+                    page.url = page.file;
+                }
+
+                page.url = page.url.replace(file.cwd + '/', '').replace(/\.(css|less|scss|sass)/g, '.html');
+
+                page.title = page.file.replace(file.cwd + '/', '').replace(/\.(css|less|scss|sass)/g, '').split('/').filter(function (item) { return item !== ''}).pop().replace(/_/g, ' ').trim();
+
+                styleguide.push(page)
+            }
 
             callback(null);
         });
     }
 
     function endStream(callback) {
-        var template =  twig({
-            path: opt.template || './templates/default/style.twig',
+        var templateCss =  twig({
+            path: opt.templates.guide || './node_modules/gulp-styleguide/templates/default/style.twig',
+            async: false
+        });
+
+        var templateHome =  twig({
+            path: opt.templates.home || './node_modules/gulp-styleguide/templates/default/home.twig',
             async: false
         });
 
         var g = this;
 
-
-
+        // Generate css guide pages
         styleguide.forEach(function (page) {
-            page.url = page.file.replace(__dirname + '/', '').replace(/\.(css|less|scss|sass)/g, '.html');
-            page.title = page.file.replace(__dirname + '/', '').replace(/\.(css|less|scss|sass)/g, '').split('/').filter(function (item) { return item !== ''}).pop();
-        });
-
-        styleguide.forEach(function (page) {
-
-            var rendercontents = template.render({blocks: page.blocks, styleguide: styleguide});
-
+            var rendercontents = templateCss.render({blocks: page.blocks, styleguide: styleguide});
             var temp = new File({ path: page.url, contents: new Buffer(rendercontents) })
             g.push(temp);
         });
+
+        //Generate index page
+        var rendercontents = templateHome.render({styleguide: styleguide});
+        var temp = new File({ path: 'index.html', contents: new Buffer(rendercontents) })
+            g.push(temp);
 
         callback();
     }
